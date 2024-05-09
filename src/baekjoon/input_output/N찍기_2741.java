16c82392a2334e7b48b5979074783b671a0b30c1
강예린 <jjykhs0505@naver.com>
2024-05-09 17:34:44 +0900

    baekjoon 2741 solved

---
+++ src/baekjoon/input_output/N찍기_2741.java
@@ -0,0 +1,13 @@
+package baekjoon.input_output;
+
+import java.util.Scanner;
+
+public class N찍기_2741 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
