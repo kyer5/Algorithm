1f0013e6488e8d5b7ba30871a2420e8e34dbe860
강예린 <jjykhs0505@naver.com>
2024-05-09 17:35:10 +0900

    baekjoon 1924 solved

---
+++ src/baekjoon/input_output/년2007_1924.java
@@ -0,0 +1,17 @@
+package baekjoon.input_output;
+
+import java.util.Scanner;
+
+public class 년2007_1924 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        String result = "";
+
+        switch (month) {
+            case 1:
+
+        }
+    }
+}
