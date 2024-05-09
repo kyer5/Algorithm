3c92239982a14ee4b01bd99d4f9647d88e4827e8
강예린 <jjykhs0505@naver.com>
2024-05-09 17:33:53 +0900

    baekjoon 11021 solved

---
+++ src/baekjoon/input_output/APlusB_7_11021.java
@@ -0,0 +1,16 @@
+package baekjoon.input_output;
+
+import java.util.Scanner;
+
+public class APlusB_7_11021 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+
+        for (int i = 0; i < testCase; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #" + (i + 1) + ": " + (a + b));
+        }
+    }
+}
