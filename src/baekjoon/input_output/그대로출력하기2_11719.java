1a2492bc7ef95355e03bc4adf6e491472544e952
강예린 <jjykhs0505@naver.com>
2024-05-09 17:34:20 +0900

    baekjoon 11719 solved

---
+++ src/baekjoon/input_output/그대로출력하기2_11719.java
@@ -0,0 +1,14 @@
+package baekjoon.input_output;
+
+import java.util.Scanner;
+
+public class 그대로출력하기2_11719 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+    }
+}
