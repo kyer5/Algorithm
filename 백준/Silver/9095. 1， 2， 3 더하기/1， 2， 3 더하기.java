08fb9f3a5fc494af0aa52f7694ffd658c0b17595
강예린 <105547387+kyer5@users.noreply.github.com>
2024-08-10 13:50:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 112 ms, Memory: 12840 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,23 +1,25 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
 
-        for (int t = 0; t < T; t++) {
+        for (int i = 0; i < T; i++) {
             int n = sc.nextInt();
-            ArrayList<Integer> num = new ArrayList<>();
-            num.add(1);
-            num.add(2);
-            num.add(4);
+            Integer[] dp = new Integer[n + 1];
 
-            for (int i = 3; i < n; i++) {
-                int a = num.get(i - 1) + num.get(i - 2) + num.get(i - 3);
-                num.add(a);
+            dp[0] = 1;
+            dp[1] = 1;
+
+            if (n > 1 && n < 11) {
+                dp[2] = 2;
+                for (int j = 3; j < n + 1; j++) {
+                    dp[j] = dp[j - 3] + dp[j - 2] + dp[j - 1];
+                }
             }
-            System.out.println(num.get(n - 1));
+            System.out.println(dp[n]);
         }
     }
 }
