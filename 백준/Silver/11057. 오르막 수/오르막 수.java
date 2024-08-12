80a6483c4e4d3c15f369ba35989da70210638b0e
강예린 <105547387+kyer5@users.noreply.github.com>
2024-08-12 23:55:07 +0900

    [Silver I] Title: 오르막 수, Time: 88 ms, Memory: 12916 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -8,12 +8,12 @@ public class Main {
         long[][] dp = new long[n + 1][10];
 
         for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1;
+            dp[0][i] = 1;
         }
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 1; i < n; i++) {
             for (int j = 0; j < 10; j++) {
-                for (int k = 0; k <= j; k++) {
+                for (int k = j; k < 10; k++) {
                     dp[i][j] += dp[i - 1][k];
                     dp[i][j] %= 10007;
                 }
@@ -22,7 +22,7 @@ public class Main {
 
         long sum = 0;
         for (int i = 0; i < 10; i++) {
-            sum += dp[n][i];
+            sum += dp[n - 1][i];
             sum %= 10007;
         }
 
