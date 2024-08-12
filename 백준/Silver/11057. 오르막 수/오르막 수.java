dfcc299ac654f08fe810aee004b8e42578e7eafe
강예린 <105547387+kyer5@users.noreply.github.com>
2024-08-12 23:53:47 +0900

    [Silver I] Title: 오르막 수, Time: 92 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[][] dp = new long[n + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = 0; k <= j; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                    dp[i][j] %= 10007;
+                }
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += dp[n][i];
+            sum %= 10007;
+        }
+
+        System.out.println(sum);
+    }
+}
