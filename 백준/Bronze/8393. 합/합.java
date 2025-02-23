55d317f25f4edf323887b7a97b4f2e29e945c092
강예린 <yerin7786@gmail.com>
2025-02-24 00:56:02 +0900

    [Bronze V] Title: 합, Time: 96 ms, Memory: 12832 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        if (n < 1 || n > 10000) {
+            System.out.println("잘못된 입력입니다.");
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
