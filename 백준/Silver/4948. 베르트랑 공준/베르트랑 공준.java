6af831f4c00597bf1fe1ab1d674bc7411be312f4
강예린 <yerin7786@gmail.com>
2025-02-25 17:59:41 +0900

    [Silver II] Title: 베르트랑 공준, Time: 160 ms, Memory: 24224 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -1,49 +1,37 @@
 import java.io.*;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        int n = 1;
-        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int n = Integer.parseInt(br.readLine());
 
-        while (n != 0) {
-            n = Integer.parseInt(br.readLine());
-            if (n != 0) {
-                list.add(n);
+            if (n == 0) {
+                break;
             }
+
+            boolean[] isPrime = new boolean[2 * n + 1];
+
+            isPrime[0] = isPrime[1] = true;
+
+            for (int i = 2; i * i <= 2 * n; i++) {
+                if (!isPrime[i]) {
+                    for (int j = i * i; j <= 2 * n; j += i) {
+                        isPrime[j] = true;
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (!isPrime[i]) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
         }
-
-        int[] countArr = new int[list.size()];
-
-        for (int i = 0; i < list.size(); i++) {
-            boolean[] isPrime = new boolean[2 * list.get(i) + 1];
-
-            isPrime[0] = true;
-            isPrime[1] = true;
-
-            for (int j = 2; j * j < isPrime.length; j++) {
-                if (isPrime[j]) {
-                    continue;
-                }
-
-                for (int k = j * j; k < isPrime.length; k += j) {
-                    isPrime[k] = true;
-                }
-            }
-
-            for (int j = list.get(i) + 1; j < isPrime.length; j++) {
-                if (!isPrime[j]) {
-                    countArr[i]++;
-                }
-            }
-            sb.append(countArr[i]).append("\n");
-        }
-
-        System.out.println(sb);
-        br.close();
     }
 }
