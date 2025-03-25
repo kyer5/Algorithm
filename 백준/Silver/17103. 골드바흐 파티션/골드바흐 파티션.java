12c23a8bdff4b8b24409246027af5419a241bba7
강예린 <yerin7786@gmail.com>
2025-03-25 21:58:01 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 168 ms, Memory: 12900 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.java
@@ -4,37 +4,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb;
 
-        int T = Integer.parseInt(br.readLine());
-        int[] NArr = new int[T];
+        boolean[] isPrimes = new boolean[1000001];
+        isPrimes[0] = isPrimes[1] = true;
 
-        for (int i = 0; i < T; i++) {
-            NArr[i] = (Integer.parseInt(br.readLine()));
-        }
-
-        for (int i = 0; i < T; i++) {
-            sb = new StringBuilder();
-            boolean[] isPrimes = new boolean[NArr[i] + 1];
-            isPrimes[0] = isPrimes[1] = true;
-            int count = 0;
-
-            for (int j = 2; j <= NArr[i]; j++) {
-                for (int k = j + j; k <= NArr[i]; k += j) {
-                    if (!isPrimes[k]) {
-                        isPrimes[k] = true;
-                    }
+        for (int i = 2; i <= Math.sqrt(1000000); i++) {
+            for (int j = i + i; j < isPrimes.length; j += i) {
+                if (!isPrimes[j]) {
+                    isPrimes[j] = true;
                 }
             }
+        }
 
-            for (int j = 2; j <= NArr[i] / 2; j++) {
-                if (!isPrimes[j] && !isPrimes[NArr[i] - j] && j + NArr[i] - j == NArr[i]) {
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int count = 0;
+            int N = Integer.parseInt(br.readLine());
+            for (int j = 2; j <= N / 2; j++) {
+                if (!isPrimes[j] && !isPrimes[N - j]) {
                     count++;
                 }
             }
 
-            sb.append(count);
-            System.out.println(sb);
+            System.out.println(count);
         }
     }
 }
