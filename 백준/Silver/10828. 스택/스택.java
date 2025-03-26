52835570b4eee2b7b81aaa799a14c182af0d004f
강예린 <yerin7786@gmail.com>
2025-03-27 00:10:23 +0900

    [Silver IV] Title: 스택, Time: 288 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -5,42 +5,31 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+
         Stack<Integer> stackInt = new Stack<>();
+        int N = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
 
             switch (input[0]) {
                 case "push":
-                    int x = Integer.parseInt(input[1]);
-                    stackInt.push(x);
+                    stackInt.push(Integer.valueOf(input[1]));
                     break;
-
                 case "pop":
-                    if (!stackInt.isEmpty()) {
-                        System.out.println(stackInt.pop());
-                    } else {
-                        System.out.println(-1);
-                    }
+                    System.out.println(stackInt.isEmpty() ? -1 : stackInt.pop());
                     break;
-
                 case "size":
                     System.out.println(stackInt.size());
                     break;
-
                 case "empty":
                     System.out.println(stackInt.isEmpty() ? 1 : 0);
                     break;
-
                 case "top":
-                    if (!stackInt.isEmpty()) {
-                        System.out.println(stackInt.peek());
-                    } else {
-                        System.out.println(-1);
-                    }
+                    System.out.println(stackInt.isEmpty() ? -1 : stackInt.peek());
                     break;
             }
         }
+
     }
 }
