3f0ed9ad5f1dd543e206cdd79317f0df639e65c7
강예린 <yerin7786@gmail.com>
2025-09-11 16:12:56 +0900

    Title: 구간 중 최대 합, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 코드트리/1차/4일차/구간 중 최대 합.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 0;
+        for (int i = 0; i < N - K + 1; i++) {
+            int sum = 0;
+            for (int j = 0; j < K; j++) {
+                sum += arr[i + j];
+            }
+            if (max < sum) max = sum;
+        }
+
+        System.out.println(max);
+    }
+}
