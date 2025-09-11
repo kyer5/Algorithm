f6b01b76045cbdd5097b7abae1374c6b3dd06880
강예린 <yerin7786@gmail.com>
2025-09-11 16:21:28 +0900

    Title: 숫자들의 합 중 최대, Time: 94ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 코드트리/1차/5일차/숫자들의 합 중 최대.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        int maxSum = 0;
+        for (int i = x; i <= y; i++) {
+            maxSum = Math.max(maxSum, digitSum(i));
+        }
+
+        System.out.println(maxSum);
+    }
+
+    public static int digitSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
