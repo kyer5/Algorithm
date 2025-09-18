e451ab65d7124445aa6a8cd8e58c81072b9bb45e
강예린 <yerin7786@gmail.com>
2025-09-18 18:05:44 +0900

    Title: 블럭쌓는 명령2, Time: 231ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 코드트리/1차/2일차/01. 블럭쌓는 명령2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] blocks = new int[n];
+
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for (int j = a - 1; j < b; j++) {
+                blocks[j]++;
+            }
+        }
+
+        int max = Arrays.stream(blocks).max().getAsInt();
+
+        System.out.println(max);
+    }
+}
