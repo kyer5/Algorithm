552605f5bdd746cb738deb086b9118b67b908983
강예린 <yerin7786@gmail.com>
2025-09-19 20:30:12 +0900

    Title: 특정 조건에 맞게 K개 중에 1개를 N번 뽑기, Time: 219ms, Memory: 18MB, Status: Passed - Codetree

---
+++ 코드트리/2차/2일차/02. 특정 조건에 맞게 K개 중에 1개를 N번 뽑기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int k, n;
+    public static int[] arr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void print() {
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+        sb.append("\n");
+    }
+
+    public static void recursion(int depth) {
+        if (depth == n) {
+            print();
+            return;
+        }
+
+        for (int i = 1; i <= k; i++) {
+            if (depth >= 2 && arr[depth - 1] == arr[depth - 2] && arr[depth - 2] == i) {
+                continue;
+            }
+            arr[depth] = i;
+            recursion(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+
+        recursion(0);
+        System.out.println(sb);
+    }
+}
