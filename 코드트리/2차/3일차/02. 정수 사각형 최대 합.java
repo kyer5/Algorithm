1134110f0978923e4ae7a5bb0a8688f4b490fb93
강예린 <yerin7786@gmail.com>
2025-09-22 20:16:36 +0900

    Title: 정수 사각형 최대 합, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 코드트리/2차/3일차/02. 정수 사각형 최대 합.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int[][] matrix;
+    public static int[][] dp;
+
+    public static void initialize() {
+        dp[0][0] = matrix[0][0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i - 1][0] + matrix[i][0];
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[0][i - 1] + matrix[0][i];
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        matrix = new int[n][n];
+        dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        initialize();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j] + matrix[i][j], dp[i][j - 1] + matrix[i][j]);
+            }
+        }
+
+        System.out.println(dp[n - 1][n - 1]);
+    }
+}
