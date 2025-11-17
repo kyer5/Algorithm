287e29c3262c143f3c84faf1d6109534c5894565
강예린 <yerin7786@gmail.com>
2025-11-18 03:22:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 10.90 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
 
 ### 성능 요약
 
-메모리: 94.7 MB, 시간: 11.84 ms
+메모리: 73.4 MB, 시간: 10.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 07일 23:30:10
+2025년 11월 18일 03:22:05
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,20 +1,14 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        String[] str = s.split(" ");
-        int[] arr = new int[str.length];
-
-        for (int i = 0; i < str.length; i++) {
-            arr[i] = Integer.parseInt(str[i]);
+        String[] arr = s.split(" ");
+        List<Integer> list = new ArrayList<>();
+        
+        for (String a : arr) {
+            list.add(Integer.parseInt(a));
         }
-
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i : arr) {
-            max = Math.max(max, i);
-            min = Math.min(min, i);
-        }
-
-        return min + " " + max;
+        
+        return (Collections.min(list) + " " + Collections.max(list));
     }
 }
