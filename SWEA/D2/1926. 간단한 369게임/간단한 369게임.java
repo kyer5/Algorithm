5ccf9f1ba3484ecdaeeb8343bb50e0217e14e746
강예린 <yerin7786@gmail.com>
2025-11-21 11:13:41 +0900

    [D2] Title: 간단한 369게임, Time: 110 ms, Memory: 27,648 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -8,21 +8,19 @@ class Solution
 		int T = sc.nextInt();
 
 		for(int i = 1; i <= T; i++) {
-            if (!String.valueOf(i).contains("3") && !String.valueOf(i).contains("6") && !String.valueOf(i).contains("9")) {
-                System.out.print(i + " ");
-            } else {
-                String[] numbers = String.valueOf(i).split("");
-                int count = 0;
-                for (int j = 0; j < numbers.length; j++) {
-                	if (numbers[j].contains("3")) {
-                    	count++;
-                    } else if (numbers[j].contains("6")) {
-                    	count++;
-                    } else if (numbers[j].contains("9")) {
-                    	count++;
-                    }
+            String s = String.valueOf(i);
+            int count = 0;
+            
+            for (char c : s.toCharArray()) {
+            	if (c == '3' || c == '6' || c == '9') {
+                	count++;
                 }
-                for (int j = 0; j < count; j++) {
+            }
+            
+            if (count == 0) {
+            	System.out.print(i + " ");
+            } else {
+            	for (int j = 0; j < count; j++) {
                 	System.out.print("-");
                 }
                 System.out.print(" ");
