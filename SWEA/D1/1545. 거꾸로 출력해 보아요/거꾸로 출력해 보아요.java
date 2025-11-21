fc2ffc33e6b35e4d5901b2f5a39929a9c9604fe2
강예린 <yerin7786@gmail.com>
2025-11-21 13:25:23 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 94 ms, Memory: 26,240 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for(int i = n; i >= 0; i--)
+		{
+            System.out.print(i + " ");
+		}
+	}
+}
