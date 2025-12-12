6e425cab34376f4915db5b58bac6c5d1c5473a7e
강예린 <yerin7786@gmail.com>
2025-12-12 23:13:38 +0900

    Title: 세 실수의 반올림, Time: 132ms, Memory: 12MB, Status: Passed - Codetree

--- 251212/세 실수의 반올림/rounding-of-three-actual-numbers.java
+++ 251212/세 실수의 반올림/rounding-of-three-actual-numbers.java
@@ -3,12 +3,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         float a = sc.nextFloat();
         float b = sc.nextFloat();
         float c = sc.nextFloat();
 
-        System.out.println(Math.round(a * 1000) / 1000.0);
-        System.out.println(Math.round(b * 1000) / 1000.0);
-        System.out.println(Math.round(c * 1000) / 1000.0);
+        System.out.printf("%.3f\n%.3f\n%.3f", a, b, c);
     }
 }
