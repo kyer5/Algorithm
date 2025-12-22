02c54a0331a6f2a1517582275071167c75ecfb58
강예린 <yerin7786@gmail.com>
2025-12-22 23:53:57 +0900

    Title: 물의 상태, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251222/물의 상태/state-of-water.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int water = sc.nextInt();
+
+        if (water < 0) {
+            System.out.println("ice");
+        } else if (water >= 100) {
+            System.out.println("vapor");
+        } else {
+            System.out.println("water");
+        }
+    }
+}
