fd4e90778deec78e131e43101460c5eb5896aaf9
강예린 <yerin7786@gmail.com>
2026-04-01 22:00:45 +0900

    [Silver III] Title: 소수 구하기, Time: 152 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,38 +1,33 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        String[] input = br.readLine().split(" ");
+        int m = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
 
-        boolean[] prime = new boolean[N + 1];
+        boolean[] isNotPrime = new boolean[n + 1];
+        isNotPrime[0] = isNotPrime[1] = true;
 
-        prime[0] = true;
-        prime[1] = true;
-
-        for (int i = 2; i * i < prime.length; i++) {
-            if (prime[i]) {
-                continue;
-            }
-
-            for (int j = i * i; j < prime.length; j += i) {
-                prime[j] = true;
+        for (int i = 2; i * i <= n; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isNotPrime[j] = true;
+                }
             }
         }
 
-        for (int i = M; i <= N; i++) {
-            if (!prime[i]) {
-                sb.append(i).append("\n");
+        StringBuilder sb = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (!isNotPrime[i]) {
+                sb.append(i).append('\n');
             }
         }
 
         System.out.println(sb);
-        br.close();
     }
 }
