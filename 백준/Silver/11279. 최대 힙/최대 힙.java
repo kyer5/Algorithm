07db319997620622188567861a2e929604edd80f
강예린 <yerin7786@gmail.com>
2026-04-04 16:12:49 +0900

    [Silver II] Title: 최대 힙, Time: 1108 ms, Memory: 34720 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+
+            if (x > 0) {
+                pq.add(x);
+            } else if (x == 0) {
+                if (!pq.isEmpty()) {
+                    System.out.println(pq.poll());
+                } else {
+                    System.out.println(0);
+                }
+            }
+        }
+    }
+}
