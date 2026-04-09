638f3e66ed16449880742b59b279dd72b74eee1b
강예린 <yerin7786@gmail.com>
2026-04-09 23:52:29 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,33 +1,28 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
+    
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
-        int num1 = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        int greatestCommonDivisor = 1;
-        int leastCommonMultiple = 1;
+        int gcd = gcd(a, b);
+        int lcm = a * b / gcd;
 
-        int min = Math.min(num1, num2);
-
-        for (int i = 2; i <= min; i++) {
-            while (num1 % i == 0 && num2 % i == 0) {
-                greatestCommonDivisor *= i;
-                num1 /= i;
-                num2 /= i;
-            }
-        }
-
-        leastCommonMultiple = greatestCommonDivisor * num1 * num2;
-
-        sb.append(greatestCommonDivisor).append("\n").append(leastCommonMultiple);
-        System.out.println(sb);
-        br.close();
+        System.out.println(gcd);
+        System.out.println(lcm);
     }
 }
