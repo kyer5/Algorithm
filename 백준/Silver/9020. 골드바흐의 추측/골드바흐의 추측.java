4437209bd997d2fcbc7c8e7a448542a394816958
강예린 <yerin7786@gmail.com>
2026-04-11 17:27:09 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 404 ms, Memory: 43744 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
@@ -11,23 +11,24 @@ public class Main {
             boolean[] isNotPrime = new boolean[num + 1];
 
             isNotPrime[0] = isNotPrime[1] = true;
-            for (int j = 2; j <= num; j++) {
-                for (int k = j * j; k <= num; k += j) {
-                    isNotPrime[k] = true;
-                }
-            }
-            int cha = Integer.MAX_VALUE;
-            int a = 0;
-            int b = 0;
-            for (int j = 2; j <= num / 2; j++) {
-                if (!isNotPrime[j] && !isNotPrime[num - j]) {
-                    if (cha > Math.abs(num - 1 - j)) {
-                        cha = Math.abs(num - 1 - j);
-                        a = j;
-                        b = num - j;
+            for (int j = 2; j * j <= num; j++) {
+                if (!isNotPrime[j]) {
+                    for (int k = j * j; k <= num; k += j) {
+                        isNotPrime[k] = true;
                     }
                 }
             }
+            
+            int a = 0;
+            int b = 0;
+            
+            for (int j = 2; j <= num / 2; j++) {
+                if (!isNotPrime[j] && !isNotPrime[num - j]) {
+                    a = j;
+                    b = num - j;
+                }
+            }
+            
             System.out.println(a + " " + b);
         }
     }
