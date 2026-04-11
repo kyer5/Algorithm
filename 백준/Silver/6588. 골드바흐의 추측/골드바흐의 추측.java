06fd5f879d04c99f5701a7467bb76f3c91c44739
강예린 <yerin7786@gmail.com>
2026-04-11 17:50:25 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 464 ms, Memory: 37904 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,45 +1,42 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb;
+        StringBuilder sb = new StringBuilder();
 
-        boolean[] isPrimeArray = new boolean[1000000];
+        boolean[] isNotPrime = new boolean[1000001];
 
-        isPrimeArray[0] = isPrimeArray[1] = true;
-
-        for (long i = 2; i < isPrimeArray.length; i++) {
-            if (!isPrimeArray[(int) i]) {
-                for (long j = i * i; j < isPrimeArray.length; j += i) {
-                    isPrimeArray[(int) j] = true;
+        isNotPrime[0] = isNotPrime[1] = true;
+        for (int i = 2; i * i <= 1000000; i++) {
+            if (!isNotPrime[i]) {
+                for (int j = i * i; j <= 1000000; j += i) {
+                    isNotPrime[j] = true;
                 }
             }
         }
 
         while (true) {
-            sb = new StringBuilder();
-            int n = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
 
-            if (n == 0) {
+            if (num == 0) {
                 break;
             }
 
-            if (n % 2 == 1) {
-                sb.append("Goldbach's conjecture is wrong.");
-                System.out.println(sb);
-                continue;
-            }
+            int a = 0;
+            int b = 0;
 
-            for (int j = 3; j <= n / 2; j++) {
-                if (!isPrimeArray[j] && !isPrimeArray[n - j]) {
-                    sb.append(n).append(" = ").append(j).append(" + ").append(n - j);
+            for (int i = 3; i <= num / 2; i += 2) {
+                if (!isNotPrime[i] && !isNotPrime[num - i]) {
+                    a = i;
+                    b = num - i;
                     break;
                 }
             }
-            System.out.println(sb);
+
+            sb.append(num).append(" = ").append(a).append(" + ").append(b).append("\n");
         }
-        br.close();
+        System.out.println(sb);
     }
 }
