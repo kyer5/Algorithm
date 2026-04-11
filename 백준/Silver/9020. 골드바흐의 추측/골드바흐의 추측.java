9736d9606c59115673bad4fffe0db8bdea5ec2d6
강예린 <yerin7786@gmail.com>
2026-04-11 17:23:15 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 484 ms, Memory: 43020 KB -BaekjoonHub

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,41 +1,34 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            int num = Integer.parseInt(br.readLine());
+            boolean[] isNotPrime = new boolean[num + 1];
 
-        for (int i = 0; i < T; i++) {
-            int n = Integer.parseInt(br.readLine());
-
-            boolean[] isPrime = new boolean[n + 1];
-
-            isPrime[0] = isPrime[1] = true;
-
-            for (int j = 2; j * j < n; j++) {
-                if (!isPrime[j]) {
-                    for (int k = j * j; k < n; k += j) {
-                        isPrime[k] = true;
+            isNotPrime[0] = isNotPrime[1] = true;
+            for (int j = 2; j <= num; j++) {
+                for (int k = j * j; k <= num; k += j) {
+                    isNotPrime[k] = true;
+                }
+            }
+            int cha = Integer.MAX_VALUE;
+            int a = 0;
+            int b = 0;
+            for (int j = 2; j <= num / 2; j++) {
+                if (!isNotPrime[j] && !isNotPrime[num - j]) {
+                    if (cha > Math.abs(num - 1 - j)) {
+                        cha = Math.abs(num - 1 - j);
+                        a = j;
+                        b = num - j;
                     }
                 }
             }
-
-            int prime1 = 0;
-            int prime2 = 0;
-
-            for (int j = 2; j <= n / 2; j++) {
-                if (!isPrime[j] && !isPrime[n - j]) {
-                    prime1 = j;
-                    prime2 = n - j;
-                }
-            }
-
-            sb.append(prime1).append(" ").append(prime2).append("\n");
+            System.out.println(a + " " + b);
         }
-        System.out.println(sb);
-        br.close();
     }
 }
