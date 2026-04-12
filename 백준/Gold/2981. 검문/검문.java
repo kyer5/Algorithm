690453f266e7f124b5a48a3c60206818c9c2895e
강예린 <yerin7786@gmail.com>
2026-04-12 17:56:29 +0900

    [Gold IV] Title: 검문, Time: 100 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Gold/2981. 검문/검문.java
+++ 백준/Gold/2981. 검문/검문.java
@@ -1,32 +1,7 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(br.readLine());
-
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(arr);
-
-        int gcdVal = arr[1] - arr[0];
-
-        for (int i = 2; i < N; i++) {
-            gcdVal = gcd(gcdVal, arr[i] - arr[i - 1]);
-        }
-
-        for (int i = 2; i <= gcdVal; i++) {
-            if (gcdVal % i == 0) {
-                System.out.print(i + " ");
-            }
-        }
-    }
 
     public static int gcd(int a, int b) {
         while (b != 0) {
@@ -35,5 +10,46 @@ public class Main {
             b = r;
         }
         return a;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);
+
+        int g = arr[1] - arr[0];
+
+        for (int i = 2; i < n; i++) {
+            g = gcd(g, arr[i] - arr[i - 1]);
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 2; i * i <= g; i++) {
+            if (g % i == 0) {
+                list.add(i);
+                if (i != g / i) {
+                    list.add(g / i);
+                }
+            }
+        }
+
+        list.add(g);
+
+        Collections.sort(list);
+
+        StringBuilder sb = new StringBuilder();
+        for (int x : list) {
+            sb.append(x).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
