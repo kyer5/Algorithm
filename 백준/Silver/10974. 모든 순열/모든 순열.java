b8ad19b1dea433d692d89163349b1b9a41c71969
강예린 <yerin7786@gmail.com>
2026-04-13 16:41:17 +0900

    [Silver III] Title: 모든 순열, Time: 244 ms, Memory: 22380 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,38 +1,44 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    static boolean[] visit;
+
+    static int n;
+    static int[] result;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        visit = new boolean[N];
-
-        backtracking(0);
-        System.out.println(sb);
-    }
-
-    public static void backtracking(int depth) {
-        if (depth == N) {
-            for (int i : arr) {
-                sb.append(i).append(" ");
+    public static void dfs(int depth) {
+        if (depth == n) {
+            for (int num : result) {
+                sb.append(num).append(" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i + 1;
-                backtracking(depth + 1);
-                visit[i] = false;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = i + 1;
+
+                dfs(depth + 1);
+
+                visited[i] = false;
             }
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        result = new int[n];
+        visited = new boolean[n];
+
+        dfs(0);
+
+        System.out.println(sb);
     }
 }
