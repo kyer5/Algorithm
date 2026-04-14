52b2198487a383e588fb8e305668b8525d78e055
강예린 <yerin7786@gmail.com>
2026-04-14 20:44:22 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 304 ms, Memory: 25684 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
@@ -1,37 +1,31 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int S = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            int location = Integer.parseInt(st.nextToken());
-            arr[i] = Math.abs(S - location);
-        }
-
-        int gcd = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            gcd = gcd(gcd, arr[i]);
+        int gcd = 0;
+        for (int i = 0; i < n; i++) {
+            int p = Integer.parseInt(st.nextToken());
+            gcd = gcd(gcd, Math.abs(s - p));
         }
 
         System.out.println(gcd);
-    }
-
-    public static int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        } else {
-            return gcd(b, a % b);
-        }
     }
 }
